51e4d0156b672178bf3f8f2de1057edcf55b5dd3
dedicad <marius.ambayrac@supelec.fr>
2021-04-07 19:22:57 +0200

    fix-test: missing provider for task

--- back/src/subjects/subjects.controller.spec.ts
+++ back/src/subjects/subjects.controller.spec.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Subject } from './entities/subject.entity';
+import { Task } from './entities/task.entity';
 import { SubjectsController } from './subjects.controller';
 import { SubjectsService } from './subjects.service';
 
@@ -14,6 +15,10 @@ describe('SubjectsController', () => {
       providers: [
         {
           provide: getRepositoryToken(Subject),
+          useClass: Repository,
+        },
+        {
+          provide: getRepositoryToken(Task),
           useClass: Repository,
         },
         SubjectsService,
--- back/src/subjects/subjects.service.spec.ts
+++ back/src/subjects/subjects.service.spec.ts
@@ -2,6 +2,8 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Subject } from './entities/subject.entity';
+import { Task } from './entities/task.entity';
+import { SubjectsController } from './subjects.controller';
 import { SubjectsService } from './subjects.service';
 
 describe('SubjectsService', () => {
@@ -12,6 +14,10 @@ describe('SubjectsService', () => {
       providers: [
         {
           provide: getRepositoryToken(Subject),
+          useClass: Repository,
+        },
+        {
+          provide: getRepositoryToken(Task),
           useClass: Repository,
         },
         SubjectsService,
