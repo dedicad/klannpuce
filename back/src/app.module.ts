bfd64d6138e1fca5838a7519970d3ec4c09cfaad
dedicad <marius.ambayrac@supelec.fr>
2021-04-07 18:45:53 +0200

    add subject and task associated

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { SubjectsModule } from './subjects/subjects.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), UsersModule],
+  imports: [TypeOrmModule.forRoot(), UsersModule, SubjectsModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ back/src/subjects/dto/create-subject.dto.ts
@@ -0,0 +1,7 @@
+import { CreateTaskDto } from './create-task.dto';
+
+export class CreateSubjectDto {
+    name: string;
+    description: string;
+    tasks: CreateTaskDto[];
+}
