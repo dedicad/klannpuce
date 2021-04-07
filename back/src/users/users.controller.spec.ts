c62ca0862428bef05c6642e8a0d6d7c21e80ec2c
dedicad <marius.ambayrac@supelec.fr>
2021-04-07 17:24:22 +0200

    refactor: reorganize user entity

--- back/src/users/users.controller.spec.ts
+++ back/src/users/users.controller.spec.ts
@@ -1,7 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { User } from './user.entity';
+import { User } from './entities/user.entity';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
