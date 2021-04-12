246be8f089b885476bb961e4e8fbdb8487f4ba34
dedicad <marius.ambayrac@supelec.fr>
2021-04-12 12:25:04 +0200

    remove prettier errors

--- back/src/main.ts
+++ back/src/main.ts
@@ -1,10 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({origin: 'http://localhost:3000'}); // Only one domain allowed, localhost. This would need to be done with more granularity and security on a real app. This is only safe for local development.
+  app.enableCors({ origin: 'http://localhost:3000' }); // Only one domain allowed, localhost. This would need to be done with more granularity and security on a real app. This is only safe for local development.
   await app.listen(8000);
 }
 bootstrap();
