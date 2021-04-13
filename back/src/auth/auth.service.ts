8a884476628a5b4808bc7288f28f4554c7ae4ace
dedicad <marius.ambayrac@supelec.fr>
2021-04-14 01:08:14 +0200

    fix: forbid access when user not exists with a clean error

--- back/src/auth/auth.service.ts
+++ back/src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
 import * as bcrypt from 'bcrypt';
 import { JwtService } from '@nestjs/jwt';
@@ -14,8 +14,9 @@ export class AuthService {
 
   async validateUser(email: string, password: string): Promise<any> {
     const user = await this.usersService.findOne(email);
+    if (!user)  throw new HttpException('BadRequestException', HttpStatus.BAD_REQUEST);
     const isMatch = await bcrypt.compare(password, user?.passwordHash);
-    if (user && isMatch) {
+    if (isMatch) {
       const { passwordHash, ...result } = user;
       return result;
     }
