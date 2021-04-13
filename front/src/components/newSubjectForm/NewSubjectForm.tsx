81bdfe3bcb55b0a4f9ed7377f6ecbe7f092d92c4
Robin Mouchart <Robin.Mouchart@gmail.com>
2021-04-13 17:00:32 +0200

    Rebase commit 2

--- front/src/components/newSubjectForm/NewSubjectForm.tsx
+++ front/src/components/newSubjectForm/NewSubjectForm.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import { useForm } from 'react-hook-form';
 import { Paper, TextField, Button, Typography, Grid, IconButton } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import DeleteIcon from '@material-ui/icons/Delete';
+
+const axios = require('axios');
+
 const useStyles = makeStyles((theme) => ({
     layout: {
         width: 'auto',
@@ -17,11 +21,6 @@ const useStyles = makeStyles((theme) => ({
         marginTop: theme.spacing(3),
         marginBottom: theme.spacing(3),
         padding: theme.spacing(2),
-        // [theme.breakpoints.up(600 + theme.spacing(3) * 2)]: {
-        //     marginTop: theme.spacing(6),
-        //     marginBottom: theme.spacing(6),
-        //     padding: theme.spacing(3),
-        // },
     },
 }));
 
@@ -29,6 +28,7 @@ const useStyles = makeStyles((theme) => ({
 export default function NewSubjectForm() {
     const classes = useStyles();
 
+    // Dynamic fields
     const [taskIdCounter, setTaskIdCounter] = React.useState(1)
     const [tasksState, setTasks] = React.useState([0])
 
@@ -48,6 +48,49 @@ export default function NewSubjectForm() {
         }))
     };
 
+    // Form
+    // Note: data not sanitysed
+    const { register, handleSubmit } = useForm();
+
+    const onSubmit = (data: any) => {
+        let tasks: any = [];
+        Object.keys(data).filter(function (k) {
+            return k.indexOf('Task-') === 0;
+        }).map(key => {
+            tasks.push({
+                "name": data[key],
+                "description": "Not necessary",
+                "level": "0"
+            })
+            return null
+        })
+
+        const reqBody = {
+            "name": data["SubjectTitle"],
+            "description": "Not necessary",
+            "tasks": tasks
+        };
+
+        const config = {
+            method: 'post',
+            url: 'http://localhost:8000/subjects',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            data: reqBody
+        };
+
+        axios(config)
+            .then(function (response: any) {
+                console.log(JSON.stringify(response.data));
+            })
+            .catch(function (error: any) {
+                console.log(error);
+            });
+
+    }
+
+
     return (
         <main className={classes.layout}>
             <Paper className={classes.paper}>
@@ -55,39 +98,46 @@ export default function NewSubjectForm() {
                     <Typography variant="h6" gutterBottom>
                         Add a new subject
                     </Typography>
-                    <Grid container spacing={3}>
-                        <Grid item xs={12} sm={12}>
-                            <TextField
-                                required
-                                id="SubjectTitle"
-                                name="SubjectTitle"
-                                label="Subject Title"
-                                fullWidth
-                            />
-                        </Grid>
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <Grid container spacing={3}>
+                            <Grid item xs={12} sm={12}>
+                                <TextField
+                                    required
+                                    id="SubjectTitle"
+                                    label="Subject Title"
+                                    fullWidth
+                                    {...register('SubjectTitle')}
+                                />
+                            </Grid>
 
-                        {tasksState.map((taskId) => {
-                            return (
-                                <React.Fragment key={taskId}>
-                                    <Grid item xs={12} sm={10}>
-                                        <TextField id={`Task-${taskId}`} size="small" label={`Task`} fullWidth></TextField>
-                                    </Grid>
-                                    <Grid item xs={12} sm={1}>
-                                        <IconButton id={`delete-${taskId}`} aria-label="delete" color="secondary" onClick={deleteTask}>
-                                            <DeleteIcon />
-                                        </IconButton>
-                                    </Grid>
-                                </React.Fragment>
-                            )
-                        })}
+                            {tasksState.map((taskId) => {
+                                return (
+                                    <React.Fragment key={taskId}>
+                                        <Grid item xs={12} sm={10}>
+                                            <TextField
+                                                id={`Task-${taskId}`}
+                                                size="small" label={`Task`}
+                                                fullWidth
+                                                {...register(`Task-${taskId}`)}
+                                            />
+                                        </Grid>
+                                        <Grid item xs={12} sm={1}>
+                                            <IconButton id={`delete-${taskId}`} aria-label="delete" color="secondary" onClick={deleteTask}>
+                                                <DeleteIcon />
+                                            </IconButton>
+                                        </Grid>
+                                    </React.Fragment>
+                                )
+                            })}
 
-                        <Grid item xs={3}>
-                            <Button fullWidth color="primary" variant="contained" onClick={addTask}>Add task</Button>
+                            <Grid item xs={3}>
+                                <Button fullWidth color="primary" variant="contained" onClick={addTask}>Add task</Button>
+                            </Grid>
+                            <Grid item xs={12}>
+                                <Button type="submit" fullWidth color="primary" variant="contained">Add Subject</Button>
+                            </Grid>
                         </Grid>
-                        <Grid item xs={12}>
-                            <Button fullWidth color="primary" variant="contained">Add Subject</Button>
-                        </Grid>
-                    </Grid>
+                    </form>
                 </React.Fragment>
             </Paper>
         </main>
